95265e2ead5d25cdb8a81038c083a307f970846e
samigold <samigold8650@gmail.com>
2026-05-17 00:47:36 +0100

    fix: externalize MongoDB and RabbitMQ configurations in application.properties

--- delivery-service/src/main/java/com/quicknotify/delivery_service/config/MongoConfig.java
+++ delivery-service/src/main/java/com/quicknotify/delivery_service/config/MongoConfig.java
@@ -2,6 +2,7 @@ package com.quicknotify.delivery_service.config;
 
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.core.MongoTemplate;
@@ -9,9 +10,12 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 @Configuration
 public class MongoConfig {
 
+    @Value("${spring.data.mongodb.uri}")
+    private String mongoUri;
+
     @Bean
     public MongoClient mongoClient() {
-        return MongoClients.create("mongodb://mongo:mongo123@localhost:27017/notification_db?authSource=admin");
+        return MongoClients.create(mongoUri);
     }
 
     @Bean
