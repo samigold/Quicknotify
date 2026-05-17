d147d652be4b238545f2bcc065e8d130ac96a162
samigold <samigold8650@gmail.com>
2026-05-17 18:59:52 +0100

    fix: mock all external dependencies in context load test

--- delivery-service/src/test/java/com/quicknotify/delivery_service/DeliveryServiceApplicationTests.java
+++ delivery-service/src/test/java/com/quicknotify/delivery_service/DeliveryServiceApplicationTests.java
@@ -2,12 +2,28 @@ package com.quicknotify.delivery_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.data.mongodb.core.MongoTemplate;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+    "spring.rabbitmq.host=localhost",
+    "spring.rabbitmq.port=5672",
+    "spring.rabbitmq.username=admin",
+    "spring.rabbitmq.password=admin123",
+    "spring.data.mongodb.uri=mongodb://localhost:27017/test",
+    "resend.api.key=test-key",
+    "resend.from.email=test@example.com"
+})
 class DeliveryServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @MockBean
+    private MongoTemplate mongoTemplate;
 
+    @MockBean
+    private ConnectionFactory connectionFactory;
+
+    @Test
+    void contextLoads() {
+    }
 }
